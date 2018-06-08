b4cd8e55b408023089d3bfb0dd909211002a6a10
Michael Weiss <mweiss001@gmail.com>
2018-06-08 06:56:47 -0600

    more web docs auto create lhs nav panel for web docs
    Signed-off-by: Michael Weiss <mweiss001@gmail.com>

---
+++ web/update_lhs_nav.pl
@@ -0,0 +1,26 @@
+# this updates the left hand side navigation panel in all html files in the current directory
+
+# get the new text
+open(MDATA, "<lhs_nav.html");
+my $b = do {local $/; <MDATA>};
+
+# iterate all .html files
+@files = <*.html>;
+foreach $file (@files)
+{
+   # print $file . "\n";
+
+   # open the file to replace text in
+   open(DATA, "<", $file);
+   my $a = do {local $/; <DATA>};
+
+   # replace the text
+   $a =~ s/<mdw_lhs_nav>(.*)<\/mdw_lhs_nav>/<mdw_lhs_nav>$b<\/mdw_lhs_nav>/s;
+ 
+   # save the changes to file
+   open(DATA, ">", $file);
+   print(DATA $a);
+}
+
+
+
